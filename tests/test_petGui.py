4cca099b5f54b59a7051025f3fe065f3e6635a38
mingyanghe <346390810@qq.com>
2023-04-11 21:45:06 +0200

    import cookie

--- tests/test_petGui.py
+++ tests/test_petGui.py
@@ -31,7 +31,7 @@ from fastapi_sessions.session_verifier import SessionVerifier
 from fastapi_sessions.frontends.implementations import SessionCookie, CookieParameters
 import uuid
 from unittest.mock import MagicMock
-
+from app.petGui import cookie
 
 class TestServer:
     @pytest.fixture()
