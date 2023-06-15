6d37e572012d2ef0e4df11919498e47e24f761c8
mingyanghe <346390810@qq.com>
2023-06-15 23:14:19 +0200

    fixed bugs on counting

--- static/js/script_add_input.js
+++ static/js/script_add_input.js
@@ -1,30 +1,49 @@
-
 var counter = 1;
 var dynamicInput_1 = [];
 
- function addInput(){
+function addInput_new() {
     var newdiv = document.createElement('div');
-    newdiv.id = dynamicInput_1[counter];
-    newdiv.innerHTML = "Template " + (counter + 1) + " <br><input class='input--style-5 form-control' type='text' placeholder='Next template text' name = template_"+counter +" required> <br><input class='btn btn--radius-2 btn--blue' style='padding: 0 25px;' type='button' value='-' onClick='removeInput("+dynamicInput_1[counter]+");'>";
-    document.getElementById('formulario').appendChild(newdiv);
+    var currentCounter = counter;
+    newdiv.id = "template_" + currentCounter;
+    newdiv.innerHTML = "Template " + (currentCounter + 1) + " <br><div class=\"input-group\"><input class='input--style-5 form-control' type='text' placeholder='A test that it is changing' name='template_" + currentCounter + "' required> <br><button class='btn btn--radius-2 btn--blue' style='padding: 0 25px;' type='button' value='-' onClick='removeInput(\"template_" + currentCounter + "\");'>-</button></div>";
+    document.getElementById('template input').appendChild(newdiv);
+    dynamicInput_1.push("template_" + currentCounter);
     counter++;
 }
 
-  function removeInput(id){
+function removeInput(id) {
     var elem = document.getElementById(id);
-    counter--;
-    return elem.parentNode.removeChild(elem);
-  }
+    elem.parentNode.removeChild(elem);
 
-  var counter_map = 2;
+    if (id.startsWith("template_")) {
+        var index = dynamicInput_1.indexOf(id);
+        if (index > -1) {
+            dynamicInput_1.splice(index, 1);
+        }
+        counter--;
+    } else if (id.startsWith("mapping_")) {
+        var index = dynamicInput.indexOf(id);
+        if (index > -1) {
+            dynamicInput.splice(index, 1);
+        }
+        counter_map--;
+    }
+}
+
+
+
+var counter_map = 2;
 var dynamicInput = [];
 
-
-function addInput_map(){
+function addInput_map() {
     var newdiv = document.createElement('div');
-    newdiv.id = dynamicInput[counter_map];
-    newdiv.innerHTML = "Mapping " + (counter_map + 1) +
-    " <div class='row row-space'><div class='col-2'><div class='input-group'><input class='input--style-5 form-control' type='text' placeholder='origin' name=origin_"+ counter_map +" required></div></div><div class='col-2'><div class='input-group'><input class='input--style-5 form-control' type='text' placeholder='mapping' name=mapping_"+counter_map+" required><input class='btn btn--radius-2 btn--blue' style='padding: 0 25px' type='button' value='-' onClick='removeInput("+dynamicInput[counter]+");'></div></div></div>";
+    var currentCounter = counter_map;
+    newdiv.id = "mapping_" + currentCounter;
+    newdiv.innerHTML = "Mapping " + (currentCounter + 1) + " <div class='row row-space'><div class='col-2'><div class='input-group'><input class='input--style-5 form-control' type='text' placeholder='origin' name='origin_" + currentCounter + "' required></div></div><div class='col-2'><div class='input-group'><input class='input--style-5 form-control' type='text' placeholder='mapping' name='mapping_" + currentCounter + "' required><input class='btn btn--radius-2 btn--blue' style='padding: 0 25px' type='button' value='-' onClick='removeInput(\"mapping_" + currentCounter + "\");'></div></div></div>";
     document.getElementById('formularmap').appendChild(newdiv);
+    dynamicInput.push("mapping_" + currentCounter);
     counter_map++;
 }
+
+
+
